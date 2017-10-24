762216018bb17c2496403c64fcd75902c380f637
Sksjti <33027875+Sksjti@users.noreply.github.com>
2017-10-24 21:32:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).scroll(function() {
+    if (window.innerWidth >= 768) {
+        var scrollPos = $(window).scrollTop();
+        if (scrollPos > 180) {
+            $('#navigation').removeClass('bg-dark');
+            $('#navigation').removeClass('initial-nav-block');
+            $('#navigation').addClass('final-nav-block');
+        } else if (scrollPos <= 180) {
+            $('#navigation').removeClass('final-nav-block');
+            $('#navigation').addClass('initial-nav-block');
+            $('#navigation').addClass('bg-dark');
+        }
+    }
+});
+
+$(window).resize(function() {
+    if (window.innerWidth < 768) {
+        if ($('#navigation').hasClass('final-nav-block')) {
+            $('#navigation').removeClass('final-nav-block');
+            $('#navigation').addClass('initial-nav-block');
+            $('#navigation').addClass('bg-dark');
+        }
+    }
+});
+
+$(function() {
+    $(".carousel").carousel({
+        interval: 2000
+    }); 
+});
