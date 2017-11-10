53aca955c4c5ae346b548a3e585cfa28168c9054
Sksjti <33027875+Sksjti@users.noreply.github.com>
2017-11-10 18:07:17 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,87 +1,63 @@
 $(function () {
     var navPos = $('#navigation').offset().top;
+    var navHeight = $('#navigation').height();
     var lastScroll = 0;
+    var nav = $('#navigation');
     $(document).scroll(function() {
-//        if (window.innerWidth >= 768) {
-////            var scrollPos = $(window).scrollTop();
-//            var scrollPos = $(window).scrollTop();
-//            if (scrollPos > 180) {
-//                $('#navigation').removeClass('bg-dark');
-//                $('#navigation').removeClass('initial-nav-block');
-//                $('#navigation').addClass('final-nav-block');
-//            } else if (scrollPos <= 180) {
-//                $('#navigation').removeClass('final-nav-block');
-//                $('#navigation').addClass('initial-nav-block');
-//                $('#navigation').addClass('bg-dark');
-//            }
-//        }
         
         if (window.innerWidth >= 768) {
             var curScroll = $(this).scrollTop();
-            var nav = $('#navigation');
-            
-            if (curScroll > lastScroll || curScroll <= navPos) {
+            if (curScroll <= navPos) {
                 nav.removeClass('final-nav-block');
+                nav.removeClass('nav-move');
+                nav.removeClass('rise-nav-block');
                 nav.addClass('initial-nav-block');
-                
-            } else if (curScroll < lastScroll) {
+            } else if (curScroll > navPos + navHeight) {
+                //hidden navbar
                 nav.removeClass('initial-nav-block');
-                nav.addClass('final-nav-block');
+                if (curScroll > lastScroll) {
+                    //down
+                    nav.removeClass('final-nav-block');
+                    nav.addClass('rise-nav-block');
+                    nav.addClass('nav-move');
+                    nVisible = false;
+                } else if (curScroll < lastScroll) {
+                    //up
+                    nVisible = true;
+                    nav.removeClass('rise-nav-block');
+                    nav.removeClass('nav-move');
+                    nav.addClass('final-nav-block');
+                } else {
+                    //none
+                }
             } else {
-                
+                //navbar visible
             }
-            
-//            var nVisible = true;
-//            if (curScroll > navPos) {
-//                //hidden navbar
-//                console.log("Nav Hidden");
-////                nav.removeClass("initial-nav-block");
-////                nav.removeClass("bg-dark");
-////                nav.addClass('final-nav-block');
-//                
-//                if (curScroll > lastScroll) {
-//                    //down
-//                    console.log("Nav Visible " + nVisible);
-//                    console.log("Down");
-//                    nVisible = false;
-//                } else if (curScroll < lastScroll) {
-//                    //up
-//                    console.log("Up");
-//                    nVisible = true;
-//                } else {
-//                    //none
-//                }
-//            } else {
-//                //navbar visible
-//                nVisible = true;
-//                console.log("Nav Visible");
-//            }
-            
             lastScroll = curScroll;
         }
-        
-        
     });
     
+    
     function setHeaderHeight() {
-
         $('header').css("height", ($('.banner').outerHeight() + $('nav').outerHeight()) + "px");
     }
 
-    window.onload = function () {
-        if (window.innerWidth < 768) {
-            $('header').css("height", "auto");
-        } else {
-            setHeaderHeight();
-        }
+    
+    //Should have executed after window loads
+    //-------------------------------------------------
+    if (window.innerWidth < 768) {
+        $('header').css("height", "auto");
+    } else {
+        setHeaderHeight();
     }
+    //-------------------------------------------------
     
     $(window).resize(function() {
         if (window.innerWidth < 768) {
-            if ($('#navigation').hasClass('final-nav-block')) {
-                $('#navigation').removeClass('final-nav-block');
-                $('#navigation').addClass('initial-nav-block');
-            }
+            nav.removeClass('final-nav-block');
+            nav.removeClass('rise-nav-block');
+            nav.removeClass('nav-move');
+            nav.addClass('initial-nav-block');
             $('header').css("height", "auto");
         } else {
             setHeaderHeight();
